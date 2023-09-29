2b65bacaeb8fd65100370f1bb87788af3088f3bb
ceastld <1950946176@qq.com>
2023-09-29 17:15:34 +0800

    test

---
+++ download_models.sh
@@ -0,0 +1,35 @@
+cd models
+
+curl 'https://civitai-delivery-worker-prod-2023-09-01.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/26957/model/realisticVisionV20.ghPF.safetensors?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22realisticVisionV51_v20Novae.safetensors%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=2fea663d76bd24a496545da373d610fc/20230929/us-east-1/s3/aws4_request&X-Amz-Date=20230929T063232Z&X-Amz-SignedHeaders=host&X-Amz-Signature=68b2e7419e46c638e4547127534209de47f5b51679ad5d0bb915ab7a9842ab50' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: zh-CN,zh;q=0.9' \
+  -H 'Connection: keep-alive' \
+  -H 'Referer: https://civitai.com/' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  --output realisticVisionV20_v20.safetensors \
+  --compressed
+
+curl 'https://civitai-delivery-worker-prod-2023-09-01.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/63217/model/rev11c.t2c3.safetensors?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22revAnimated_v11.safetensors%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=2fea663d76bd24a496545da373d610fc/20230929/us-east-1/s3/aws4_request&X-Amz-Date=20230929T062858Z&X-Amz-SignedHeaders=host&X-Amz-Signature=101afab4ea820c1856ee7739eddd05f5466e0319ff552c8a17d863dc1f449bfa' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: zh-CN,zh;q=0.9' \
+  -H 'Connection: keep-alive' \
+  -H 'Referer: https://civitai.com/' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  --output revAnimated_v11.safetensors \
+  --compressed
---
+++ test.sh
@@ -0,0 +1,2 @@
+# CUDA_VISIBLE_DEVICES="5" python rerender.py --cfg config/real2sculpture.json
+CUDA_VISIBLE_DEVICES="5" python rerender.py --cfg config/id1.json
